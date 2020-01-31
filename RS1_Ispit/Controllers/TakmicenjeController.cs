695843217eee22f208eaf2defab0fcd766afbd06
emrahokic <emrahokic96@hotmail.com>
2020-01-31 22:58:13 +0100

    .

--- RS1_Ispit/Controllers/TakmicenjeController.cs
+++ RS1_Ispit/Controllers/TakmicenjeController.cs
@@ -12,42 +12,7 @@ using RS1_Ispit_asp.net_core.ViewModels;
 
 namespace RS1_Ispit_asp.net_core.Controllers
 {
-    //View Model za pocetnu formu za izbor skole i razreda 
-    public  class TakmicenjeFormVM
-    {
-        public int SkolaID { get; set; }
-        public string Razred { get; set; }
-        public List<SelectListItem> Skole { get; set; }
-        public List<SelectListItem> Razredi { get; set; }
-
-        public TakmicenjeFormVM()
-        {
-            Skole = new List<SelectListItem>();
-            Razredi = new List<SelectListItem>();
-
-            //Dodavanje razreda u listu za DropDown
-            Razredi.Add(new SelectListItem()
-            {
-                Text = "Odaberite Razred",
-                Value = null,
-                Disabled = true,
-                Selected = true
-            });
-            Razredi.Add(new SelectListItem()
-            {
-                Text = "Svi razredi",
-                Value = "0"
-            });
-            for (int i = 1; i < 5; i++)
-            {
-                Razredi.Add(new SelectListItem()
-                {
-                    Value = i.ToString(),
-                    Text = i.ToString(),
-                });
-            }
-        }
-    }
+    
 
     public class TakmicenjeController : Controller
     {
---
+++ RS1_Ispit/ViewModels/TakmicenjeFormVM.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class TakmicenjeFormVM
+    {
+        public int SkolaID { get; set; }
+        public string Razred { get; set; }
+        public List<SelectListItem> Skole { get; set; }
+        public List<SelectListItem> Razredi { get; set; }
+
+        public TakmicenjeFormVM()
+        {
+            Skole = new List<SelectListItem>();
+            Razredi = new List<SelectListItem>();
+
+            //Dodavanje razreda u listu za DropDown
+            Razredi.Add(new SelectListItem()
+            {
+                Text = "Odaberite Razred",
+                Value = null,
+                Disabled = true,
+                Selected = true
+            });
+            Razredi.Add(new SelectListItem()
+            {
+                Text = "Svi razredi",
+                Value = "0"
+            });
+            for (int i = 1; i < 5; i++)
+            {
+                Razredi.Add(new SelectListItem()
+                {
+                    Value = i.ToString(),
+                    Text = i.ToString(),
+                });
+            }
+        }
+    }
+}
